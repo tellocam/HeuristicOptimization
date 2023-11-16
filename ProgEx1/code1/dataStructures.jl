e2eeeb9e03776eb43cce9a616f73ed21fe5290f5
tellocam <tellocam92@gmail.com>
2023-11-16 11:25:19 +0100

    added wrong code for deltaEval() for symmetric matrices.. gonna change that soon..

--- ProgEx1/code1/dataStructures.jl
+++ ProgEx1/code1/dataStructures.jl
@@ -1,16 +1,20 @@
 using LinearAlgebra
 
-mutable struct graphData
+struct graphData
     n::Int8
     m::Int8
     l::Int8
     A0::Symmetric{Int8, Matrix{Int8}}  # initial adjacency matrix
     A::Symmetric{Int8, Matrix{Int8}}   # current adjacency matrix
     W::Symmetric{Int8, Matrix{Int8}}   # weight matrix
+    cf_0::Int8 # Initial cost
+    cf_actual::Int8
+    cf_min::Int8 # Minimal cost so far
 end
 
-function read_instance_file(file_path_rel::AbstractString)
-# read file
+
+function readGraphFile(file_path_rel::AbstractString)
+    # read file
     # Construct the absolute path by combining the script's directory with the relative path
     script_dir = @__DIR__
     file_path = joinpath(script_dir, file_path_rel)
@@ -37,22 +41,39 @@ function read_instance_file(file_path_rel::AbstractString)
         end
     end
 
-    # Only keep the lower (or upper) triangular part by using symmetric matrices
+    # make matrices symmetric thereby it doesnt matter if we addres edge by i,j or j,i
     A0 = Symmetric(A)
-    G = graphData(n, m, l, A0, copy(A0), Symmetric(W))
-
+    W0 = Symmetric(W)
+    f0 = sum(tril(A0) .* tril(W0)) # frobenius product on lower triangular matrices A and W for inital cost.
+    G = graphData(n, m, l, A0, copy(A0), W0, f0, f0, f0)
+    
     return G
 end
 
-# # Provide the path to your instance file
-# file_path = "datasets/inst_test/heur001_n_10_m_31.txt"
+function deltaEval(G::graphData, i, j)
+    modifiedA = copy(G.A)
+    modifiedA[i,j] = abs(G.A[i,j] - 1) # flip 1->0 or 0->1
 
-# # Call the function to generate matrices
-# G_test = read_instance_file(file_path)
+    if G.A[i,j] # check if edge is present initially
+        added_cost = -G.W[i,j] # deduct cost of edge if yes
+    else # add cost if edge is not present
+        added_cost = G.W[i,j]
+    end
+
+    deltaCost = G.cf_actual + added_cost
+    return graphData(G.n, G.m, G.l, G.A0, modifiedA, G.W, G.cf_0, deltaCost, G.cf_min)
+end
+
+# Provide the path to your instance file
+file_path = "datasets/inst_test/heur001_n_10_m_31.txt"
+
+# Call the function to generate matrices
+G_test = readGraphFile(file_path)
 
 # # Display the matrices
 # println("Adjacency Matrix:")
-# display(G_test.A0)
+display(G_test.cf_min)
+deltaEval(G_test, 4,8)
 
 # println("\nWeight Matrix:")
 # display(G_test.W)
