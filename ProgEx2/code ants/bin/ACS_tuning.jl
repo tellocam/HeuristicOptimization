a180214495f8bd3df5e7646813b280430e3dae11
tellocam <tellocam92@gmail.com>
2024-01-11 08:29:02 +0100

    tuning file comments

--- ProgEx2/code ants/bin/ACS_tuning.jl
+++ ProgEx2/code ants/bin/ACS_tuning.jl
@@ -1,34 +1,25 @@
 include("../src/ACS_fun.jl")
 include("../bin/ACS.jl")
 
-num_trials = 30
+
 instance_folder_path = "../data/datasets/inst_tuning_selected/"
 data_folder_path = "../data"
-num_files = 8
 
 
-# # Example usage
-# num_trials = 5  # Change this to the desired number of trials
-# instance_folder_path = "../data/datasets/inst_tuning/"
-# num_files = 1
-
-# Define fixed values for parameters
-α_values = [0.2, 0.25, 0.3, 0.35, 0.4]
+num_trials = 30 # number of combinations tested
+num_files = 8 # number of files selected randomly out of folder. if 8 then all files!
+α_values = [0.2, 0.25, 0.3, 0.35, 0.4] # discrete value list for parameters
 μ_values = [0.2, 0.25, 0.3, 0.35, 0.4]
 q0_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
-# Generate all combinations
+# Generate combinations
 all_combinations = collect(Iterators.product(α_values, μ_values, q0_values))
+println(length(all_combinations)) #prints the number of combinations that are available of which num_trials will be tested
 
-# # Call the tuning function with the combinations
-# best_params, best_avg_result = random_search_ACS_tuning_with_combinations(num_trials, all_combinations, instance_folder_path, num_files)
-
-# # ... (rest of the code remains the same)
-
-
+# # Call tuning function with combinations
 best_params, best_avg_result = random_search_ACS_tuning(num_trials, all_combinations, instance_folder_path, num_files)
 
-# Append num_trials, num_files, best_params, and best_avg_result to the text file
+# Append num_trials, num_files, best_params, and best_avg_result to text file
 output_file = joinpath(data_folder_path, "reported_parameters.txt")
 open(output_file, "a") do file
     println(file, "\n---------------------------")
